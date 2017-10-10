646e8099cdaeec51cc01b01ae26803a45eb38647
zubair khan <zubair_khan@Live.com>
2017-10-10 16:34:01 -0700

    Adding more monitor tests

---
+++ tests/FRAMEWORK-MONITOR-003.js
@@ -0,0 +1,52 @@
+/**
+ * WPETestFramework test
+ */
+/*jslint esnext: true*/
+
+module.exports = {
+    'title'             : 'Framework Monitor tests',
+    'description'       : 'Tests if the Framework Monitor module returns measurement values for YouTube',
+    'requiredPlugins'   : ['Monitor', 'YouTube'],
+    'steps'         : {
+        'step1' : {
+            'description'   : 'Stop Monitor module',
+            'test'          : stopPlugin,
+            'params'        : 'Monitor',
+            'validate'      : httpResponseSimple,
+        },
+        'step2' : {
+            'sleep'         : 10,
+            'description'   : 'Check if Monitor is stopped succesfully',
+            'test'          : getPluginState,
+            'params'        : 'Monitor',
+            'assert'        : 'deactivated'
+        },
+        'step3' : {
+            'description'   : 'Start Monitor module',
+            'test'          : startPlugin,
+            'params'        : 'Monitor',
+            'validate'      : httpResponseSimple,
+        },
+        'step4' : {
+            'sleep'         : 10,
+            'description'   : 'Check if Monitor is started succesfully',
+            'test'          : getPluginState,
+            'params'        : 'Monitor',
+            'assert'        : 'activated'
+        },
+        'step5' : {
+            'description'   : 'Get Plugin Data',
+            'test'          : getPlugin,
+            'params'        : 'Monitor',
+            'validate'      : (resp) => {
+                var monitor = JSON.parse(resp.body);
+                
+                console.log(monitor[1].name);
+                if (monitor[1].measurement === undefined)
+                	throw new Error('Monitor measurement data is not present for YouTube');
+                return true;
+            }
+        }
+    },
+    'cleanup'       : restartFramework
+};
