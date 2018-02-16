a5a8285744c8b99f580b98abdf3a09cc1472b04f
wouterlucas <wouter@wouterlucas.com>
2018-02-15 16:14:54 -0800

    cleaning up old structure

--- core/config.js
+++
@@ -1,5 +0,0 @@
-/**
- * WPETestFramework configuration file
- */
-exports.server = '127.0.0.1'
-exports.port = 1337;
--- tests/DUMMY-ERROR-006.js
+++
@@ -1,9 +0,0 @@
-/**
- * WPETestFramework dummy test task
- */
-/*jslint esnext: true*/
-
-module.exports = {
-    'title'         : 'Dummy Test Error',
-    'description'   : 'This tests has no tasks',
-};
