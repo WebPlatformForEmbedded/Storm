9cf49d811548d26734b32bd73504500d7efb731f
Michiel van der Geest <hello@michielvandergeest.com>
2019-03-07 11:51:17 -0300

    Fresh start.

--- build/js/tests/DUMMY-ERROR-006.js
+++
@@ -1,8 +0,0 @@
-/**
- * WPETestFramework dummy test task
- */
-
-test = {
-    'title'         : 'Dummy Test Error',
-    'description'   : 'This tests has no tasks',
-};
--- src/js/conf.js
+++
@@ -1,6 +0,0 @@
-/**
- * WPE Test Framework configuration file
- */
-conf = {
-    'refresh_interval'  : 5000
-};
