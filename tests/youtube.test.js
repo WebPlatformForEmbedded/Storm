f4bc3140df8dd0782ee009ebaa9cd7900e8cbbda
Michiel van der Geest <hello@michielvandergeest.com>
2019-03-08 13:35:25 -0300

    Implemented timeout of 5 seconds to the steps in the youtube test.

--- tests/youtube.test.js
+++ tests/youtube.test.js
@@ -6,12 +6,14 @@ module.exports = {
     steps: [
         {
             description: 'Stop webkit browser',
+            timeout: 5,
             test: helpers.tests.stopPlugin,
             params: 'WebKitBrowser',
             validate: helpers.validators.httpSuccess,
         },
         {
             description: 'Stop Youtube',
+            timeout: 5,
             test: helpers.tests.stopPlugin,
             params: 'YouTube',
             validate: helpers.validators.httpSuccess,
@@ -19,6 +21,7 @@ module.exports = {
         },
         {
             description: 'Start Youtube',
+            timeout: 5,
             test: helpers.tests.startPlugin,
             params: 'YouTube',
             validate: helpers.validators.httpSuccess,
@@ -26,6 +29,7 @@ module.exports = {
         },
         {
             description: 'Set Youtube URL',
+            timeout: 5,
             test: helpers.tests.setPluginUrl,
             params: ['YouTube', 'https://www.youtube.com/tv#/watch/video/control?v=RDfjXj5EGqI'],
             validate: helpers.validators.httpSuccess,
@@ -33,6 +37,7 @@ module.exports = {
         },
         {
             description: 'Resume Youtube',
+            timeout: 5,
             test: helpers.tests.resumePlugin,
             params: 'YouTube',
             validate: helpers.validators.httpSuccess,
@@ -40,6 +45,7 @@ module.exports = {
         },
         {
             description: 'Check CPU load',
+            timeout: 5,
             test: helpers.tests.getCpuLoad,
             validate: (cpuLoad) => cpuLoad <= 90,
             sleep: 5,
