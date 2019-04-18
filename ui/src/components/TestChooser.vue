b7fa88d35d417e381e497bae198e7829b529793c
Michiel van der Geest <hello@michielvandergeest.com>
2019-04-18 13:52:45 -0300

    Fixed TestChooser to properly 'remember' selected tests, even when navigating to other areas.

--- ui/src/components/TestChooser.vue
+++ ui/src/components/TestChooser.vue
@@ -1,21 +1,21 @@
 <template>  
   <div>
-       <div class="w-full border-b border-light-grey mb-4 pb-2 flex" v-for="(test, index) in tests" :key="'test' + index">
-           <div class="w-3/5">
-            <h2 class="text-base text-black w-full mb-2">{{test.title}}</h2>
-            <p class="text-sm text-dark-grey mb-2">{{test.description}}</p>
-            <p class="text-sm text-dark-grey mb-2">
-                <span class="font-bold">{{test.steps.length}}</span> steps
-            </p>
-           </div>
-           <div class="w-2/5 text-right">
-            <button 
-              class="ml-2 bg-blue hover:bg-dark-blue text-white text-xs font-bold p-2 rounded focus:outline-none"
-              @click="selectOrUnselectTest(test)"
-              v-text="selected(test) ? 'Unselect' : 'Select'"
-            />
-          </div>
-       </div>
+    <div class="w-full border-b border-light-grey mb-4 pb-2 flex" v-for="(test, index) in tests" :key="'test' + index">
+        <div class="w-3/5">
+        <h2 class="text-base text-black w-full mb-2">{{test.title}}</h2>
+        <p class="text-sm text-dark-grey mb-2">{{test.description}}</p>
+        <p class="text-sm text-dark-grey mb-2">
+            <span class="font-bold">{{test.steps.length}}</span> steps
+        </p>
+        </div>
+        <div class="w-2/5 text-right">
+        <button 
+          class="ml-2 bg-blue hover:bg-dark-blue text-white text-xs font-bold p-2 rounded focus:outline-none"
+          @click="selectOrUnselectTest(test)"
+          v-text="selected(test) ? 'Unselect' : 'Select'"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -24,26 +24,30 @@ import Tests from '../../../tests'
 
 export default {
   name: 'TestChooser',
-  data: () =>({
-    selectedTests: []
-  }),
   computed: {
     tests: () => Tests,
+    selectedTests() {
+      return this.$store.state.tests
+    }
   },
   methods: {
-      selected(test) {
-          const selected = this.selectedTests.find(t => t === test)
-          return !!selected
-      },
-      selectOrUnselectTest(test) {
-          this.selected(test) ? 
-            this.selectedTests = this.selectedTests.filter(t => {
-                return t !== test
-            }) :
-            this.selectedTests.push(test)
+    selected(test) {
+      const selected = this.selectedTests.find(t => t === test)
+      return !!selected
+    },
+    selectOrUnselectTest(test) {
+      const tests = this.selectedTests
 
-            this.$store.commit('SET_TESTS', this.selectedTests)
+      if(this.selected(test)) {
+        const index = tests.indexOf(test)
+        tests.splice(index, 1)
+      } 
+      else {
+        tests.push(test)
       }
+
+      this.$store.commit('SET_TESTS', tests)
+    }
   }
 }
 </script>
