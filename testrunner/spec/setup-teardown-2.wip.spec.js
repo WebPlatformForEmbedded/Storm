242109e93b40bef774999fa69d3b73de539a074f
Michiel van der Geest <hello@michielvandergeest.com>
2019-05-27 20:00:28 -0300

    Added 4 automated tests for setup-teardown testCases (1 wip).

---
+++ testrunner/spec/setup-teardown-2.wip.spec.js
@@ -0,0 +1,16 @@
+import test from 'tape-promise/tape'
+
+import Runner from '../index'
+import Reporter from '../reporters/tdd'
+
+import testCase from '../../tests/dummy/setup-teardown-2'
+
+const reporter = Reporter()
+
+test('Setup Teardown 2 - Teardown after failing step', assert => {
+  Runner(testCase, reporter).catch(res => {
+    // todo ...
+    assert.pass('To do ..')
+    assert.end()
+  })
+})
