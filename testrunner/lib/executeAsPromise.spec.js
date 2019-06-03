97a139531cb1a08e470c79becebebe3a2145773b
Michiel van der Geest <hello@michielvandergeest.com>
2019-06-03 16:53:10 -0300

    Added unit tests for executeAsPromise.

---
+++ testrunner/lib/executeAsPromise.spec.js
@@ -0,0 +1,40 @@
+import test from 'tape'
+import executeAsPromise from './executeAsPromise'
+
+test('executeAsPromise - Type', assert => {
+  let expected = 'function'
+  let actual = typeof executeAsPromise
+  assert.equal(actual, expected, 'executeAsPromise should be a function')
+
+  assert.end()
+})
+
+test('executeAsPromise - pass a promise', assert => {
+  const promise = new Promise((resolve, reject) => {
+    let rand = Math.random()
+    if (rand >= 0.5) {
+      resolve()
+    } else {
+      reject()
+    }
+  })
+  let actual = executeAsPromise(promise)
+  assert.ok(
+    typeof actual.then === 'function' && typeof actual.catch === 'function',
+    'should return a promise when passed a promise'
+  )
+
+  assert.end()
+})
+
+test('executeAsPromise - pass a normal function', assert => {
+  let actual = executeAsPromise(function() {
+    return 'ok'
+  })
+  assert.ok(
+    typeof actual.then === 'function' && typeof actual.catch === 'function',
+    'should return a promise when passed a function'
+  )
+
+  assert.end()
+})
