86ed6cb704a229ebcfffa9450801991a1de6f6a9
Michiel van der Geest <hello@michielvandergeest.com>
2019-06-06 14:20:38 -0300

    Added search functionality when selecting tests via CLI.

--- cli/cli.js
+++ cli/cli.js
@@ -1,10 +1,13 @@
 import Inquirer from 'inquirer'
+import ChexkboxPlus from 'inquirer-checkbox-plus-prompt'
 import Chalk from 'chalk'
 import Contra from 'contra'
 
 import Runner from '../testrunner'
 import Tests from '../tests'
 import Reporter from './reporter'
+
+Inquirer.registerPrompt('checkbox-plus', ChexkboxPlus)
 
 // construct a list of tests to show as options
 const listTests = tests => {
@@ -36,12 +39,31 @@ const clearConsole = () => {
 const run = async () => {
   clearConsole()
 
+  const tests = listTests(Tests)
   const questions = [
     {
-      type: 'checkbox',
+      type: 'checkbox-plus',
       name: 'TESTS',
-      message: 'Select the test(s) you want to execute',
-      choices: listTests(Tests),
+      message: [
+        [
+          'Select the test(s) you want to execute using the',
+          Chalk.yellow('spacebar'),
+          'then press',
+          Chalk.yellow('enter'),
+          'to start the Testrunner!',
+        ].join(' '),
+        [Chalk.yellow('➜'), Chalk.dim('Search by title / description:')].join(' '),
+      ].join('\n'),
+      searchable: true,
+      source: (selected, input) => {
+        return new Promise(function(resolve) {
+          resolve(
+            tests.filter(test => {
+              return test.name.toLowerCase().includes(input.toLowerCase())
+            })
+          )
+        })
+      },
       pageSize: process.stdout.rows || 20,
     },
   ]
