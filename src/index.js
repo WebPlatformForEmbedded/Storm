202218f7117cea51b5844ec5eab92b9555fef7c2
Michiel van der Geest <hello@michielvandergeest.com>
2019-08-21 16:02:09 -0300

    Enabled passing a full thunderJS config object to Storm instead of just the host.

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import Test from './lib/test'
 import reporters from './reporters'
 import ThunderJS from 'ThunderJS'
 
-export default (testCase, reporter, host) => {
+export default (testCase, reporter, thunderConfig) => {
   // if reporter not an object, map passed string to default reporter
   if (typeof reporter === 'string') {
     reporter = reporters[reporter]
   }
 
-  const thunderJS = ThunderJS({ host })
+  const thunderJS = ThunderJS(thunderConfig)
 
   return new Promise((resolve, reject) => {
     Test(testCase, reporter, thunderJS)
