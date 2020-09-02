ebb36d54a44dc519913cb63857712f72307a7c54
sandeep-vedam <sandeepkumar.vedam@soctronics.com>
2020-09-02 18:58:16 +0530

    Changes related to prompt

--- src/lib/test.js
+++ src/lib/test.js
@@ -150,6 +150,26 @@ const Mixin = function() {
         return dotObjectKey.get(this.context, key)
       }.bind(this),
     },
+    $prompt: {
+      selectChoices: function(msg, choice, waitTime) {
+        return this.prompt.selectChoices(msg, choice, waitTime)
+      },
+      enterText: function(msg, waitTime) {
+        return this.prompt.enterText(msg, waitTime)
+      },
+      selectOption: function(msg, choices, waitTime) {
+        return this.prompt.selectOption(msg, choices, waitTime)
+      },
+      enterChoiceNumber: function(message, choice, waitTime) {
+        return this.prompt.enterNumberForChoice(message, choice, waitTime)
+      },
+      enterPassword: function(message, waitTime) {
+        return this.prompt.enterPassword(message, waitTime)
+      },
+      confirm: function(message, waitTime) {
+        return this.prompt.getConfirmationFromUser(message, waitTime)
+      },
+    },
     $data: {
       read: function(key) {
         return dotObjectKey.get(this.data, key)
@@ -165,15 +185,16 @@ const Mixin = function() {
   }
 }
 
-const makeTest = (testCase, reporter, thunderJS) => {
+const makeTest = (testCase, reporter, thunderJS, prompt) => {
   const defaults = {
     data: {},
     context: {},
+    prompt: {},
   }
 
   const test = Object.assign(
     // Mixin functionality into the test case
-    Object.create(Mixin.call({ ...defaults, ...testCase, ...{ reporter, thunderJS } })),
+    Object.create(Mixin.call({ ...defaults, ...testCase, ...{ reporter, thunderJS } }), prompt),
     { ...defaults, ...testCase }
   )
 
@@ -184,6 +205,7 @@ const makeTest = (testCase, reporter, thunderJS) => {
     reporter,
     thunderJS,
     test,
+    prompt,
     exec() {
       // execue the test (multiple times depending on repeat)
       return new Promise((resolve, reject) => {
